16dd8f452404339cfd0b703ddf12dedd81f87117
muyue <muyue>
2020-09-22 21:06:20 +0800

    共享目录配置

--- src/main/java/com/zzg/mybatis/generator/plugins/DbRemarksCommentGenerator.java
+++ src/main/java/com/zzg/mybatis/generator/plugins/DbRemarksCommentGenerator.java
@@ -54,7 +54,7 @@ public class DbRemarksCommentGenerator implements CommentGenerator {
             compilationUnit.addImportedType(new FullyQualifiedJavaType("javax.persistence.Id"));
             compilationUnit.addImportedType(new FullyQualifiedJavaType("javax.persistence.Column"));
             compilationUnit.addImportedType(new FullyQualifiedJavaType("javax.persistence.GeneratedValue"));
-            compilationUnit.addImportedType(new FullyQualifiedJavaType("org.hibernate.validator.constraints.NotEmpty"));
+            compilationUnit.addImportedType(new FullyQualifiedJavaType("javax.validation.constraints.NotEmpty"));
         }
     }
 
